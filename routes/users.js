780b9122f5970773e54ae481892ae30a8f323516
Aakash Prasad <73053809+aakash-prasad@users.noreply.github.com>
2021-04-19 19:46:51 +0530

    Final registration using async

--- routes/users.js
+++ routes/users.js
@@ -12,10 +12,6 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.send('Registeration Succesful');
 })
-// const register_handler =async (req, res)=>{
-
-// }
-// router.post('/register' , register_handler)
 router.post('/register', async (req, res)=>{
     const{ firstName, lastName, email, password } = req.body;
     let errors =[];
@@ -79,75 +75,3 @@ module.exports = router;
 
 
 
-//register handle
-// router.post('/register', (req, res) => {
-//     const {
-//         firstName,
-//         lastName,
-//         email,
-//         password
-//     } = req.body;
-
-//     let errors = [];
-
-//     //checking all fields empty or not
-//     if (!firstName || !lastName || !email || !password) {
-//         errors.push({
-//             msg: "Please Fill All The Fields"
-//         })
-//     }
-//     //checking the length of the password
-//     if (password.length < 6) {
-//         errors.push({
-//             msg: "Password must be atleast 6 characters long"
-//         });
-//     }
-
-//     if (errors.length > 0) {
-//         res.send(errors);
-//         console.log('Render the form again');
-//     } else {
-//         // Validation Passed
-//         User.findOne({
-//                 email: email
-//             }
-//             
-//             .then(user => {
-//                 if (user) {
-//                     errors.push({
-//                         msg: "Email is already registered"
-//                     })
-//                     res.send("user already exist")
-//                 } else {
-//                     const newUser = new User({
-//                         firstName: firstName,
-//                         lastName: lastName,
-//                         email: email,
-//                         password: password
-//                     });
-//                     //Password Hashing:
-//                     bcrypt.hash(newUser.password, 10, function(err, hash) {
-//                         if (err) throw err;
-//                         newUser.password = hash;
-                        
-//                         newUser.save()
-//                             // return new Promise((resolve, reject)=>{
-//                             //     if(resolve) resolve(`Promise resolved, data saved`)
-//                             //     else reject(`There is an error saving the data `)
-//                             // })
-//                             const doWork = async ()=>{
-//                                 try{
-//                                     var response = await newUser.save();
-//                                     console.log(response);    
-//                                 }catch(err){console.log(err)}
-//                             }
-//                             doWork();
-                        
-//                     });
-//                 }
-//             })
-//     }
-
-// })
-
-// module.exports = router;
